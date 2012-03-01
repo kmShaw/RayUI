905ceb0c08f2fe203e2b0443c7c5b7a0cd4fcf50
kmShaw <ysxx1987@163.com>
2012-03-01 10:52:00 +0800

    Update Interface/AddOns/!RayUI/modules/actionbar/bar1.lua

--- Interface/AddOns/!RayUI/modules/actionbar/bar1.lua
+++ Interface/AddOns/!RayUI/modules/actionbar/bar1.lua
@@ -3,7 +3,7 @@ local R, C, L, DB = unpack(select(2, ...))
 local bar = CreateFrame("Frame","RayUIActionBar1",UIParent, "SecureHandlerStateTemplate")
 bar:SetWidth(C["actionbar"].buttonsize*12+C["actionbar"].buttonspacing*11)
 bar:SetHeight(C["actionbar"].buttonsize)
-bar:Point("BOTTOM", "UIParent", "BOTTOM", -3 * C["actionbar"].buttonsize -3 * C["actionbar"].buttonspacing, 235)
+bar:Point("BOTTOM", "BOTTOMInfoBar", "BOTTOM", 0, C["actionbar"].buttonsize)
 bar:SetScale(C["actionbar"].barscale)
 
 R.CreateMover(bar, "ActionBar1Mover", L["动作条1锚点"], true)
